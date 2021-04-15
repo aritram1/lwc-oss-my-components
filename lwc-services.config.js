5b142d7926147d9e7befe04a1ad65dcf264b6d53
aritram1 <aritram1@gmail.com>
2021-04-15 22:19:02 +0530

    built

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     resources: [
         { from: 'src/client/resources/', to: 'dist/resources/' },
-        { from: 'node_modules/@salesforce-ux/design-system/assets', to: 'src/SLDS' }, //to incorporate SLDS
+        // { from: 'node_modules/@salesforce-ux/design-system/assets', to: 'src/SLDS' }, //to incorporate SLDS
         { from: 'node_modules/@salesforce-ux/design-system/assets', to: 'dist/SLDS' } //to incorporate SLDS
     ],
     
